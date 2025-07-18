12894af7936ac0251ce2f8af2bb8b5284508d9eb
Gustavo <gustavovargas11m@gmail.com>
2025-07-18 17:13:54 -0500

    add class Operator and operators

---
+++ core/operator.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from typing import List
+
+@dataclass
+class Operator:
+    """ Represent a system operator who can manage incident"""
+    name: str
+    role: str
+    specialties: List[str]  # Incident types this operator can hadle
+    max_incidents: int = 3
+    assigned_incident: List[str] = field(default_factory=list)
+    available_at: datetime = field(default_factory=lambda: datetime.now())
+    status: str = "available" # or busy
+
+    def is_avaliable(self) -> bool:
+        """
+        Check if the operator is available to take a new incident.
+        Conditions:
+            - Status must be 'available'.
+            - Incident count must be below max_incidents.
+            - Current time must be past 'available_at'.
+        """
+        return (
+            self.status.lower() == "available"
+            and len(self.assigned_incident) < self.max_incidents
+            and datetime.now() >= self.available_at
+        )
+    
+    def assign_incident(self, incident_id: str, estimated_minutes: int) -> None:
+        """
+        Assign a new incident to this operator.
+        Updates:
+            - Adds incident ID.
+            - Updates availability time.
+            - Updates status if max is reached.
+        """
+        self.assign_incident.append(incident_id)
+        self.available_at = datetime.now() + timedelta(minutes=estimated_minutes)
+
+        if len(self.assign_incident) >= self.max_incidents:
+            self.status = "busy"
+
+    def remove_incident(self, incident_id: str) -> None:
+        """
+        Remove a resolved incident and update status
+        """
+        if incident_id in self.assign_incident:
+            self.assign_incident.remove(incident_id)
+        if len(self.assign_incident) < self.max_incidents:
+            self.status = "available"
---
+++ core/operators.py
@@ -0,0 +1,35 @@
+from core.operator import Operator
+from typing import Dict
+
+OPERATORS = Dict[str, Operator] = {
+    "Alice": Operator(
+        name="Alice",
+        role="support_level_1",
+        specialties=["network", "software"],
+        max_incidents=2
+    ),
+    "Carlos": Operator(
+        name="Carlos",
+        role="admin",
+        specialties=["all"],
+        max_incidents=5
+    ),
+    "Max": Operator(
+        name="Max",
+        role="support_level_2",
+        specialties=["hardware", "database", "software"],
+        max_incidents=2
+    ),
+    "Dana": Operator(
+        name="Dana",
+        role="support_level_1",
+        specialties=["software", "ux"],
+        max_incidents=2
+    ),
+    "Eric": Operator(
+        name="Eric",
+        role="support_level_2",
+        specialties=["security", "network"],
+        max_incidents=3
+    ),
+}
